a529a20c91a6cd55323167ecede72475b2d77da6
techingsol2-jpg <techingsol2@gmail.com>
2026-04-03 03:35:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toggleTable() {
   const table = document.getElementById("tableSection");
+  const btn = document.querySelector(".compare-btn");
+
   table.classList.toggle("hidden");
 
   if (!table.classList.contains("hidden")) {
     table.scrollIntoView({ behavior: "smooth" });
+    btn.innerText = "Hide Comparison";
+  } else {
+    btn.innerText = "View Full Comparison";
   }
 }
