4edc2658d94d9bdfdd2d00537ab1531fed6bf9ff
techingsol2-jpg <techingsol2@gmail.com>
2026-04-03 02:35:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function toggleTable() {
   const table = document.getElementById("tableSection");
   table.classList.toggle("hidden");
+
+  if (!table.classList.contains("hidden")) {
+    table.scrollIntoView({ behavior: "smooth" });
+  }
 }
