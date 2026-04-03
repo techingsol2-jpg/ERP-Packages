924c0edb0e1a44d055e2b948aef389ab906598c7
techingsol2-jpg <techingsol2@gmail.com>
2026-04-03 19:44:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ canvas.style.position = "fixed";
 canvas.style.top = 0;
 canvas.style.left = 0;
 canvas.style.zIndex = 0;
-
+canvas.style.pointerEvents = "none"; // 🔥 YE LINE ADD KARO
 const ctx = canvas.getContext("2d");
 
 let particles = [];
